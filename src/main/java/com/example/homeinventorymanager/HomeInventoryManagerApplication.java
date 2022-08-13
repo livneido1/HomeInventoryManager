1490a1c4bf8548db4944d2e1b5cc444d059ee206
Ido Livne <Livneido1@gmail.com>
2022-08-13 12:04:14 +0300

    Initial commit

---
+++ src/main/java/com/example/homeinventorymanager/HomeInventoryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeinventorymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeInventoryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeInventoryManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/homeinventorymanager/HomeInventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeinventorymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeInventoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
